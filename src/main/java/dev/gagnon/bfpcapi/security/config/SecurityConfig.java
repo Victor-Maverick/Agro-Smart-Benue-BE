81e37cb1d77e70bd22a210c1c6f2163450c4b5d8
Victor <victor.orlu@bdic.ng>
2025-11-17 18:16:29 +0100

    commit

--- src/main/java/dev/gagnon/bfpcapi/security/config/SecurityConfig.java
+++ src/main/java/dev/gagnon/bfpcapi/security/config/SecurityConfig.java
@@ -49,7 +49,11 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://agrosmartbenue.com"));
+        configuration.setAllowedOrigins(List.of(
+            "http://localhost:3000", 
+            "https://agrosmartbenue.com",
+            "https://www.agrosmartbenue.com"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("*"));
